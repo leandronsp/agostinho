497133165031da77d2a918e3435c9173e0ba275a
leandronsp <leandronsp@gmail.com>
2024-02-28 19:37:06 -0300

    trying to fix transaction date

--- lib/accounts_service.rb
+++ lib/accounts_service.rb
@@ -25,7 +25,11 @@ class AccountsService
       }
 
       sql = <<~SQL
-        SELECT amount, transaction_type, description, TO_CHAR(date, 'YYYY-MM-DD HH:MI:SS.US')
+        SELECT 
+          amount, 
+          transaction_type, 
+          description, 
+          TO_CHAR(date, 'YYYY-MM-DD HH:MI:SS.US') AS date
         FROM transactions
         WHERE transactions.account_id = $1
         ORDER BY date DESC
